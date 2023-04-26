5c2f4ccd9722999bb8eeffcc1924b92d31ae70de
DiNeez <dneez86@gmail.com>
2023-04-26 16:32:55 +0300

    Got rid of classNames with undefined values in the button component

--- components/UI/button/button.jsx
+++ components/UI/button/button.jsx
@@ -6,9 +6,23 @@ const Button = ({
   variant = "primary",
   theme = "purple",
 }) => {
-  const variantClass = variant === "secondary" ? "secondary" : "primary";
-  const themeClass = theme === "teal" ? "teal" : "purple";
-  const buttonClassname = `${styles.button} ${styles[variantClass]} ${styles[themeClass]}`;
+  const variantClass = variant === "secondary" ? "secondary" : false;
+  const themeClass = theme === "teal" ? "teal" : false;
+
+  const getButtonClassName = (variant, theme) => {
+    if ([variant, theme].every(Boolean))
+      return `${styles.button} ${styles[variantClass]} ${styles[themeClass]}`;
+
+    if ([variant, theme].some(Boolean)) {
+      return variant === false
+        ? `${styles.button} ${styles[themeClass]}`
+        : `${styles.button} ${styles[variantClass]}`;
+    }
+
+    return `${styles.button}`;
+  };
+
+  const buttonClassname = getButtonClassName(variantClass, themeClass);
 
   return (
     <button className={buttonClassname} onClick={handleClick}>
