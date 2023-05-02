e7f3861325e98389e04055afae5651316485a193
DiNeez <dneez86@gmail.com>
2023-05-02 17:11:18 +0300

    format section done, but without animations for now

--- components/formats/formats.jsx
+++ components/formats/formats.jsx
@@ -1,8 +1,26 @@
-import Title from "../UI/title/title";
+import { useState } from "react";
 
+import Title from "../UI/title/title";
+import { OverlayScrollbarsComponent } from "overlayscrollbars-react";
+import { formats } from "@/data";
+
+import "overlayscrollbars/overlayscrollbars.css";
 import styles from "./formats.module.scss";
 
 const Formats = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleButtonClick = (event) => {
+    const clickedElementId = event.currentTarget.dataset.id;
+    const indexOfClickedElement = formats.findIndex(
+      (format) => format.id === clickedElementId
+    );
+
+    setCurrentIndex(indexOfClickedElement);
+  };
+
+  const FormatImage = formats[currentIndex].image;
+
   return (
     <section className={styles.formats}>
       <div className={styles.container}>
@@ -10,8 +28,57 @@ const Formats = () => {
         <p className={styles.subtitle}>
           we work with the most popular forms of advertising
         </p>
-        <div className={styles.buttons}>Buttons</div>
-        <div className={styles.showcase}>ShowCase</div>
+        <OverlayScrollbarsComponent
+          element="div"
+          options={{
+            scrollbars: { autoHide: "never" },
+          }}
+          className={`${styles.scrollbar} kakak`}
+          defer
+        >
+          <ul className={styles.buttons}>
+            {formats.map((format) => {
+              const { id, title, thumbnail: Thumbnail } = format;
+
+              return (
+                <li
+                  key={id}
+                  data-id={id}
+                  onClick={handleButtonClick}
+                  className={styles.button}
+                >
+                  <Thumbnail />
+                  <h4 className={styles.buttonTitle}>{title}</h4>
+                </li>
+              );
+            })}
+          </ul>
+        </OverlayScrollbarsComponent>
+        <div className={styles.showcase}>
+          <div className={styles.imageWraper}>
+            <FormatImage className={styles.image} />
+          </div>
+          <div>
+            <h3 className={styles.formatTitle}>
+              {formats[currentIndex].title}
+            </h3>
+            <p className={styles.descr}>{formats[currentIndex].descr}</p>
+            <div className={styles.descrFooter}>
+              <p className={styles.details}>
+                <span className={styles.detailsLabel}>Cost per click</span>
+                <span className={styles.detailsValue}>
+                  from {formats[currentIndex].costPerClick}$ CPM
+                </span>
+              </p>
+              <p className={styles.details}>
+                <span className={styles.detailsLabel}>Daily volume</span>
+                <span className={styles.detailsValue}>
+                  more than {formats[currentIndex].dailyVolume} views
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
--- data/index.js
+++ data/index.js
@@ -1,6 +1,24 @@
 import Gadgets from "@/assets/images/icons/gadgets.svg";
 import Payout from "@/assets/images/icons/payout.svg";
 import Diamond from "@/assets/images/icons/diamond.svg";
+import Banner from "@/assets/images/formats/banner.svg";
+import BannerThumbnail from "@/assets/images/formats/banner-thumbnail.svg";
+import Video from "@/assets/images/formats/video.svg";
+import VideoThumbnail from "@/assets/images/formats/video-thumbnail.svg";
+import Native from "@/assets/images/formats/native.svg";
+import NativeThumbnail from "@/assets/images/formats/native-thumbnail.svg";
+import Teaser from "@/assets/images/formats/teaser.svg";
+import TeaserThumbnail from "@/assets/images/formats/teaser-thumbnail.svg";
+import Popunder from "@/assets/images/formats/popunder.svg";
+import PopunderThumbnail from "@/assets/images/formats/popunder-thumbnail.svg";
+import SearchTraffic from "@/assets/images/formats/search-traffic.svg";
+import SearchTrafficThumbnail from "@/assets/images/formats/search-traffic-thumbnail.svg";
+import InApp from "@/assets/images/formats/in-app.svg";
+import InAppThumbnail from "@/assets/images/formats/in-app-thumbnail.svg";
+import Push from "@/assets/images/formats/push.svg";
+import PushThumbnail from "@/assets/images/formats/push-thumbnail.svg";
+import Inpage from "@/assets/images/formats/inpage.svg";
+import InpageThumbnail from "@/assets/images/formats/inpage-thumbnail.svg";
 
 export const offers = [
   {
@@ -28,83 +46,91 @@ export const offers = [
 export const formats = [
   {
     id: "format-banner-1",
-    title: "Banner Advertising",
+    title: "Banner ads",
     descr: "one of the classic advertising formats shown on the page.",
-    clickPrice: 2,
-    dailyValue: "1 billion",
-    image: "",
-    thumbnail: "",
+    costPerClick: 0.003,
+    dailyVolume: "1 billion",
+    image: Banner,
+    thumbnail: BannerThumbnail,
   },
   {
     id: "format-video-2",
-    title: "",
-    descr: "",
-    clickPrice: 2,
-    dailyValue: "1 billion",
-    image: "",
-    thumbnail: "",
+    title: "Video ads",
+    descr:
+      "Video ads are created to promote a product, service, or brand to a target audience. They can take on different formats and are effective due to their visual and auditory nature.",
+    costPerClick: 0.003,
+    dailyVolume: "1 billion",
+    image: Video,
+    thumbnail: VideoThumbnail,
   },
   {
     id: "format-native-3",
-    title: "",
-    descr: "",
-    clickPrice: 2,
-    dailyValue: "1 billion",
-    image: "",
-    thumbnail: "",
+    title: "Native ads",
+    descr:
+      "Form of advertising that blends in with the form and function of the platform where they are displayed.",
+    costPerClick: 0.003,
+    dailyVolume: "1 billion",
+    image: Native,
+    thumbnail: NativeThumbnail,
   },
   {
     id: "format-teaser-4",
-    title: "",
-    descr: "",
-    clickPrice: 2,
-    dailyValue: "1 billion",
-    image: "",
-    thumbnail: "",
+    title: "Teaser ads",
+    descr:
+      "Teaser ads are creates curiosity and anticipation by providing a glimpse of a product or service without revealing too much information.",
+    costPerClick: 0.003,
+    dailyVolume: "1 billion",
+    image: Teaser,
+    thumbnail: TeaserThumbnail,
   },
   {
     id: "format-popunder-5",
-    title: "",
-    descr: "",
-    clickPrice: 2,
-    dailyValue: "1 billion",
-    image: "",
-    thumbnail: "",
+    title: "Popunder ads",
+    descr:
+      "Popunder ads opens in a new browser window behind the current window that the user is viewing.",
+    costPerClick: 0.003,
+    dailyVolume: "1 billion",
+    image: Popunder,
+    thumbnail: PopunderThumbnail,
   },
   {
     id: "format-search-traffic-6",
-    title: "",
-    descr: "",
-    clickPrice: 2,
-    dailyValue: "1 billion",
-    image: "",
-    thumbnail: "",
+    title: "Search Traffic ads",
+    descr:
+      "Also known as Search Engine Marketing (SEM) or Pay-Per-Click (PPC) ads, are a form of online advertising that involves placing ads on search engine results pages (SERPs).",
+    costPerClick: 0.003,
+    dailyVolume: "1 billion",
+    image: SearchTraffic,
+    thumbnail: SearchTrafficThumbnail,
   },
   {
     id: "format-in-app-7",
-    title: "",
-    descr: "",
-    clickPrice: 2,
-    dailyValue: "1 billion",
-    image: "",
-    thumbnail: "",
+    title: "In-app ads",
+    descr:
+      "In-app ads appears within a mobile application. These ads can take on various forms, including banner ads, interstitial ads, and rewarded video ads, and they are designed to blend in with the app's user interface",
+    costPerClick: 0.003,
+    dailyVolume: "1 billion",
+    image: InApp,
+    thumbnail: InAppThumbnail,
   },
   {
-    id: "",
-    title: "",
-    descr: "",
-    clickPrice: 2,
-    dailyValue: "1 billion",
-    image: "",
-    thumbnail: "",
+    id: "format-push-8",
+    title: "Push ads",
+    descr:
+      "Advertising that involves sending notifications to users' devices, such as smartphones or desktops. ",
+    costPerClick: 0.003,
+    dailyVolume: "1 billion",
+    image: Push,
+    thumbnail: PushThumbnail,
   },
   {
-    id: "",
-    title: "",
-    descr: "",
-    clickPrice: 2,
-    dailyValue: "1 billion",
-    image: "",
-    thumbnail: "",
+    id: "format-inpage-9",
+    title: "In Page",
+    descr:
+      "Also known as display ads, are a form of online advertising that appear within the content of a webpage. ",
+    costPerClick: 0.003,
+    dailyVolume: "1 billion",
+    image: Inpage,
+    thumbnail: InpageThumbnail,
   },
 ];
